83cd1e14dff969bc882dce7632388fceba95ca6e
Nikita Popov <nckcol@gmail.com>
2022-12-04 15:40:22 +0200

    forth day (done) **

--- 4/index.ts
+++ 4/index.ts
@@ -12,15 +12,15 @@ function parseInput(input: string) {
     .map((pairString) => pairString.split(",").map(parseRange));
 }
 
-function contains(range1: Range, range2: Range) {
-  return range1[0] <= range2[0] && range1[1] >= range2[1];
+function intersects(range1: Range, range2: Range) {
+  return range1[0] <= range2[1] && range2[0] <= range1[1];
 }
 
 const input = await Deno.readTextFile("./input.txt");
 const pairList = parseInput(input);
 
-const result = pairList.filter(
-  ([range1, range2]) => contains(range1, range2) || contains(range2, range1)
+const result = pairList.filter(([range1, range2]) =>
+  intersects(range1, range2)
 ).length;
 
 console.log(result);
